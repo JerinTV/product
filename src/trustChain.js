e9ec26a5449c02d01c5bf460102114ca5c979922
Jerin TV <jerinjkzjkz@gmail.com>
2026-02-15 08:36:56 +0530

    message

--- src/trustChain.js
+++ src/trustChain.js
@@ -4,7 +4,7 @@ import CryptoJS from "crypto-js";
 
 /* ================= CONFIG ================= */
 
-const CONTRACT_ADDRESS = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 /* ================= PROVIDER ================= */
 
